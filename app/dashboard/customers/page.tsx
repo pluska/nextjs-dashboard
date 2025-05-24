93d6ef2134522e3757c7ac6fc779bcd7d629376b
Andres Pluska <andres.pluska@verusen.com>
2025-05-24 12:06:50 -0500

    Customers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,12 @@
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page() {
+  const customers = await fetchFilteredCustomers("");
+  return <CustomersTable customers={customers} />;
 }
